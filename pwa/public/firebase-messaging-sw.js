9e27c5a556d983afd713cf9c8730f36cafd157de
kevollex <167472898+kevollex@users.noreply.github.com>
2025-06-27 00:26:06 -0400

    without critical information

--- pwa/public/firebase-messaging-sw.js
+++ pwa/public/firebase-messaging-sw.js
@@ -7,13 +7,13 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.1/firebas
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyC1ArA65ErYtjKRkSzhjTC53Sqbj8VvdyQ",
-    authDomain: "monteapp-f96cc.firebaseapp.com",
-    projectId: "monteapp-f96cc",
-    storageBucket: "monteapp-f96cc.firebasestorage.app",
-    messagingSenderId: "1036678128041",
-    appId: "1:1036678128041:web:1b524526a5cbfcf38864b8",
-    measurementId: "G-F0P79F3RV9"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 
 // Initialize Firebase
